e19ff13782c4c77c0b7acfe77b8306020e099708
laven <jotian@deloitte.com.cn>
2024-06-07 17:45:10 +0800

    feat(service-auth): 权限查寻功能增加

--- service/service-auth/src/main/java/com/threec/auth/dao/SysUserDao.java
+++ service/service-auth/src/main/java/com/threec/auth/dao/SysUserDao.java
@@ -1,8 +1,12 @@
 package com.threec.auth.dao;
 
 import com.threec.auth.entity.SysUserEntity;
+import com.threec.auth.security.dto.AuthUrlMethodDTO;
 import com.threec.common.mybatis.dao.BaseDao;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 系统用户表
@@ -14,5 +18,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface SysUserDao extends BaseDao<SysUserEntity> {
 
     SysUserEntity findByUsername(String username);
+
+    List<AuthUrlMethodDTO> authByUsername(@Param("username")String username,@Param("method") String method);
 	
 }
