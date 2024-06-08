a17518210e8ef8420ad0073dcdf999258f6b3e5f
laven <jotian@deloitte.com.cn>
2024-06-08 15:50:21 +0800

    feat(service-auth): 新增用户模块

--- service/service-auth/src/main/java/com/threec/auth/security/constant/AuthConstant.java
+++ service/service-auth/src/main/java/com/threec/auth/security/constant/AuthConstant.java
@@ -9,7 +9,7 @@ package com.threec.auth.security.constant;
  * @since 8/6/24
  */
 public class AuthConstant {
-    public static final String[] WHITE_LIST_URL = {"/auth/error","/api/auth/**", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html"};
+    public static final String[] WHITE_LIST_URL = {"/api/auth/**", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html"};
     public static final String API_AUTH = "api/auth";
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
