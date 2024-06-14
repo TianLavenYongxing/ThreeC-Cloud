3b31b94fd3d1ce20c08d4a87749db1656922c3d3
laven <jotian@deloitte.com.cn>
2024-06-14 18:53:23 +0800

    feat: 网管模块增加

---
+++ service/service-gateway/src/main/java/com/threec/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.threec.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class GatewayApplication.
+ * <p>
+ * 网关
+ * </p>
+ *
+ * @author laven
+ * @version 1.0
+ * @since 14/6/24
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
