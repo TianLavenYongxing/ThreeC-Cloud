7996c1e09f1fb701ce4475e05f954bc37c181f4f
laven <jotian@deloitte.com.cn>
2024-06-18 12:20:11 +0800

    feat(test): 增加测试controller

---
+++ service/service-test/service-consumer/src/main/java/com/threec/consumer/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.threec.consumer.controller;
+
+import com.threec.tools.utils.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class HelloCnotroller.
+ * <p>
+ * hello
+ * </p>
+ *
+ * @author laven
+ * @version 1.0
+ * @since 18/6/24
+ */
+@RequestMapping("hello")
+@RestController
+public class HelloController {
+
+    @GetMapping()
+    public R<Object> hello(){
+        return new R<>().ok("hello");
+    }
+}
