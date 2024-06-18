6dc32da1ecf2a9bb540477d685233e6abe4b228c
laven <jotian@deloitte.com.cn>
2024-06-18 11:31:24 +0800

    refactor(auth): 代码整理

--- service/service-auth/src/main/java/com/threec/auth/security/ThreeCSecurityConfig.java
+++ service/service-auth/src/main/java/com/threec/auth/security/ThreeCSecurityConfig.java
@@ -5,18 +5,20 @@ import com.threec.auth.security.filter.JwtAuthenticationFilter;
 import com.threec.auth.security.handler.ThreeCAuthorizationHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 import javax.annotation.Resource;
 
@@ -38,8 +40,7 @@ public class ThreeCSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);         // 关闭csrf攻击防御
         http.cors(Customizer.withDefaults());               // 跨域问题
-        http.authorizeHttpRequests(authz -> authz.requestMatchers(AuthConstant.WHITE_LIST_URL).permitAll().anyRequest().access(threeCAuthorizationHandler)  //开启授权保护  anyRequest() 对所有请求开启授权保护  access()自定义授权
-        );
+        http.authorizeHttpRequests(authz -> authz.requestMatchers(AuthConstant.WHITE_LIST_URL).permitAll().anyRequest().access(threeCAuthorizationHandler));  //开启授权保护  anyRequest() 对所有请求开启授权保护  access()自定义授权
         http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 无状态配置：通过
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         http.exceptionHandling(e -> {
