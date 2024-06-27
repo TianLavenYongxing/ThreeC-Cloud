674aeeeb01e1c2f0daa64f1bd513401b48a32dc7
laven <jotian@deloitte.com.cn>
2024-06-27 18:27:21 +0800

    feat(service-test): 添加redis相关测试，为集成redis做准备

---
+++ service/service-test/service-prod/src/test/java/com/threec/prod/RedisTest.java
@@ -0,0 +1,26 @@
+package com.threec.prod;
+
+import com.threec.prod.utils.RedisUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * Class RedisTest.
+ * <p>
+ *
+ * </p>
+ *
+ * @author laven
+ * @version 1.0
+ * @since 24/6/24
+ */
+
+
+
+@SpringBootTest
+public class RedisTest {
+    @Test
+    void redisTest(){
+        RedisUtils.StringOps.set("s","women");
+    }
+}
+
